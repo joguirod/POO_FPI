87c56df49c2ec357c253a8635eff79a5b4c628b1
José Guilherme <joguicane@gmail.com>
2023-08-14 11:54:48 -0300

    código das classes

---
+++ Exercicio01/classes/Circulo/TestaCirculo.java
@@ -0,0 +1,8 @@
+public class TestaCirculo {
+    public static void main(String[] args){
+        Circulo circulo1 = new Circulo(4);
+
+        circulo1.showArea();
+        circulo1.showPerimetro();
+    }
+}
---
+++ Exercicio01/classes/SituacaoFinanceira/TestaSituacaoFinanceira.java
@@ -0,0 +1,7 @@
+public class TestaSituacaoFinanceira {
+    public static void main(String[] args) {
+        SituacaoFinanceira conta1 = new SituacaoFinanceira(1000, 300);
+
+        conta1.showSaldo();
+    }
+}
